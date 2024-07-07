fe7ea03c30e545978dd95396bfab64833a91aea0
Axel Acevedo <axelacevedo021@gmail.com>
2024-07-07 17:07:23 -0300

    first commit

---
+++ src/main/java/abm/abmJava/AbmJavaApplication.java
@@ -0,0 +1,13 @@
+package abm.abmJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abm/abmJava/AbmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package abm.abmJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
